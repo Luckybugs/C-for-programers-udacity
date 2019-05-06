fc02a7dc83bd642a4ef43dce5b18c4f2f3722368
Jelena Jovanovic <djurovic.jelena@gmail.com>
2019-05-06 15:14:22 +0100

    lesson2

--- lesson 1-basics.cpp
+++ lesson 1-basics.cpp
@@ -193,3 +193,180 @@ int main()
 
     return 0;
 }
+
+/*A sample solution to the Format Output Program. */
+#include <iostream>
+#include <iomanip>
+
+int main()
+{
+    int a = 45;
+    float b = 45.323;
+    double c = 45.5468;
+    int aa = a + 9;
+    float bb = b + 9;
+    double cc = c + 9;
+    int aaa = aa + 9;
+    float bbb = bb + 9;
+    double ccc = cc + 9;
+
+    std::cout<<"print with set width = 10\n";
+    std::cout<<"Ints"<<std::setw(10);
+    std::cout<<"Floats"<<std::setw(10);
+    std::cout<<"Doubles"<<std::setw(10) << "\n";
+
+    std::cout<< a;
+    std::cout<< std::setw(12)<< b;
+    std::cout<< std::setw(10)<< c << "\n";    
+
+    std::cout<< aa;
+    std::cout<< std::setw(12)<< bb;
+    std::cout<< std::setw(10)<< cc << "\n";  
+
+    std::cout<< aaa;
+    std::cout<< std::setw(12)<< bbb;
+    std::cout<< std::setw(10)<< ccc << "\n\n"; 
+
+    std::cout<<"print with tabs\n";
+    std::cout<<"Int"<<"\tFloats"<<"\tDoubles\n";
+    std::cout<< aaa<<"\t"<< bbb<<"\t"<< ccc <<"\n"; 
+
+    return 0;
+}
+
+
+/* IO steps:
+
+1. include the <fstream> library
+2. Create a stream (input, output, boath)
+  - ofstream myfile; (for writing to a file)
+  - ifstream myfile; (for reading a file)
+  - fstream myfile; (for reading and writing)
+3. Open the file myfile.open("filname");
+4. write or read file
+5. close the file myfile.close();
+
+*/
+
+/*The goal of this quiz is to practice writing and reading files.
+**Read the contents of input.txt and then write to it. 
+**
+**We are using input.txt as our file. This is not an ideal
+**situation, because when we write to it, we cannot
+**see the changes. We can manually write in input.txt and
+**we can also use the program to write to the file. 
+**Then we can read what we wrote using our program.
+**
+**Your assignment for this quiz**
+**Change the contents of the file called input.txt
+**Change the ifstream and ofstream to fstream
+
+*/
+#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+
+int main () {
+    string line;
+    //create an output stream to write to the file
+    //append the new lines to the end of the file
+    ofstream myfileI ("input.txt", ios::app);
+    if (myfileI.is_open())
+    {
+        myfileI << "\nI am adding a line.\n";
+        myfileI << "I am adding another line.\n";
+        myfileI.close();
+    }
+    else cout << "Unable to open file for writing";
+  
+    //create an input stream to write to the file
+    ifstream myfileO ("input.txt");
+    if (myfileO.is_open())
+    {
+        while ( getline (myfileO,line) )
+        {
+            cout << line << '\n';
+        }
+        myfileO.close();
+    }
+    
+    else cout << "Unable to open file for reading";
+    
+    return 0;
+}
+
+/* As we have seen we can include additional libraries in C++, we can also include our own libraries.
+   Traditionally, these files are called header files and they have an .hpp extension. Although any extension will work.
+
+    Header files contain information about how to do a task.
+    The main program contains information about what to do. 
+
+Let’s see how a header file works with a simple hello world program.
+We have a simple hello world program. We can test this, and the program runs.
+
+    main.cpp: all the code on what the program does goes in this file.
+
+    main.hpp: all the code on how the program does a task goes in this file.
+ 
+ Move the include statement and the using namespace statement to the header file. 
+ Now we have to tell the compiler to include the header file. 
+ So we add the line #include "main.hpp" to the main.cpp file. 
+
+ Please note … I’m using double quotes here so it looks in working dir. 
+*/
+
+/*Goal: practice using header files.
+**. 
+**Put in the header file every thing that is related to 
+**"how to do a task".
+**Put the "what to do" in the main program.
+*/
+
+#include "main.hpp"
+
+int main()
+{
+    cout<<"Hello, I use header files!";
+    return 0;
+}
+
+/*This is a header file. 
+**Put in a header file every thing that is related to 
+**"how to do a task".
+**Put the "what to do" in the main program.
+*/
+
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+
+
+
+/*Debugging Practice */
+
+#include "main.hpp"
+
+     int main ()
+      {
+        float FTemp = 0;
+        float CTemp = 0;
+
+        cout << "Enter a Farenheit temperature: ";
+        cin>>FTemp;
+
+        CTemp = (FTemp - 32.0) / (9.0/5.0);
+        cout << "\n"<<FTemp <<" in farenheit = " <<CTemp<<" in Celcius\n";
+        return 0;
+      }
+
+
+
+/*The header file: main.hpp */
+
+#include<iostream>
+
+using namespace std;
